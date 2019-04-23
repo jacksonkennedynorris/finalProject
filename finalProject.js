51869ab5c46b33f50f09aff89a80b0b2db3c4c0b
Jackson Norris <jacksonnorris@Jacksons-MacBook-Pro.local>
2019-04-23 14:50:18 -0400

    j

--- finalProject.js
+++ finalProject.js
@@ -1,1 +1,25 @@
-var dataP = d3.csv('finalProject.csv');
+var presidentPromise = d3.csv('president.csv');
+presidentPromise.then(function(data){
+  presidentMap(data)
+},
+function(err){
+  console.log(err)
+});
+
+var presidentMap = function(data){
+  console.log(data)
+  var logYear = function(d,i){
+    console.log(d)
+  }
+}
+
+
+
+
+var senatePromise = d3.csv('senate.csv')
+senatePromise.then(function(data){
+  //console.log(data)
+},
+function(err){
+  console.log(err)
+});
